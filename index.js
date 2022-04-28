2bf823b2a20531828f11a10174b5937f713f5557
duncan-nn <nnoromduncan@gmail.com>
2022-04-28 01:59:21 +0100

    make it better

--- index.js
+++ index.js
@@ -33,11 +33,7 @@ app.use("/api/orders", orderRoute);
 app.use("/api/checkout", checkoutRoute);
 app.use("/api/currency", currencyRoute);
 
-app.listen(80, () => {
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
-
-
-// app.listen(process.env.PORT || 5000, () => {
-//   console.log("Backend server is running!");
-// });
